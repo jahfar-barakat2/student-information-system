750a49bea5981363fa1fef38385f1846dd7c9ff3
jahfar barakat <jahfar.barakat@stu.fbu.edu.tr>
2026-01-02 16:41:02 +0000

    feat : Student model -Added Student struct to include Closes #29

---
+++ include/Student.hpp
@@ -0,0 +1,15 @@
+#ifndef STUDENT_HPP
+#define STUDENT_HPP
+
+#include <string>
+
+
+struct Student {
+    int id;
+    std::string name;
+    std::string surname;
+    std::string department;
+    std::string email;
+};
+
+#endif
